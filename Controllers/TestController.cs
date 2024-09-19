7ef237ee901e9b228f2750bf334f146be09f8d9a
oxi1224 <oxi1224@o2.pl>
2024-09-19 18:23:01 +0200

    Skończenie kroku 2

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
 using Zadanie_5.Models;
-
-// await using var ctx = new TestDBContext();
-// await ctx.Database.EnsureDeletedAsync();
-// await ctx.Database.EnsureCreatedAsync();
 
 namespace Zadanie_5.Controllers {
   public class TestController : Controller {
     private readonly TestDBContext ctx;
-    // GET: Test
     public TestController(TestDBContext context) {
       ctx = context;
     }
@@ -28,28 +25,11 @@ namespace Zadanie_5.Controllers {
     [HttpPost("Create")]
     public IActionResult Create(Klienci klient) {
       if (klient.PESEL.Length != 11) {
-        ModelState.AddModelError("Error", "Numer PESEL nie posiada 11 znaków");
-      } else {
-        int year = int.Parse(klient.PESEL.Substring(0, 2));
-        int month = int.Parse(klient.PESEL.Substring(2, 2));
-        
-        if (month <= 12) {
-          year += 1900;
-        } else if (month <= 32) {
-          year += 2000;
-        } else if (month <= 52) {
-          year += 2100;
-        } else if (month <= 72) {
-          year += 1800;
-        }
-        if (year != klient.BirthYear) {
-          ModelState.AddModelError("Error", "Rok urodzenia nie zgadza się z numerem PESEL");
-        } else {
-          int gender = int.Parse(klient.PESEL[9].ToString()) % 2 == 0 ? 1 : 2;
-          if (gender != klient.Płeć) {
-            ModelState.AddModelError("Error", "Płeć nie zgadza się z numerem PESEL");
-          }
-        }
+        ModelState.AddModelError("PESEL", "Numer PESEL nie posiada 11 znaków");
+      } else if (!Klienci.ValidBirthYear(klient.PESEL, klient.BirthYear)) {
+        ModelState.AddModelError("Rok urodzenia", "Rok urodzenia nie zgadza się z numerem PESEL");
+      } else if (!Klienci.ValidPłeć(klient.PESEL, klient.Płeć)) {
+        ModelState.AddModelError("Płeć", "Płeć nie zgadza się z numerem PESEL");
       }
       if (!ModelState.IsValid) return View(klient);
       ctx.Klienci.Add(klient);
@@ -87,5 +67,146 @@ namespace Zadanie_5.Controllers {
       ctx.SaveChanges();
       return RedirectToAction("Index");
     }
-  }
+
+    [HttpGet("Import")]
+    public IActionResult Import() {
+      return View();
+    }
+
+    static private readonly string[] validHeaders = { "Id", "Name", "Surname", "PESEL", "BirthYear", "Płeć" };
+    [HttpPost("Import")]
+    public IActionResult Import(IFormFile File) {
+      if (File == null || File.Length == 0) {
+        ViewBag.Error = "Nie wybrano pliku";
+        return View();
+      }
+      string extension = Path.GetExtension(File.FileName).ToLowerInvariant();
+      if (extension != ".csv" && extension != ".xlsx") {
+				ViewBag.Error = "Nieprawidłowy typ pliku, dozwolone są tylko pliki .csv i .xlsx";
+				return View();
+			}
+      if (extension == ".csv") {
+        StreamReader reader = new(File.OpenReadStream(), true);
+        string headerLine = reader.ReadLine()!.Trim();
+        string delim = headerLine.Contains(";") ? ";" : ",";
+        string[] headers = headerLine.Split(delim);
+				if (!Enumerable.SequenceEqual(headers, validHeaders)) {
+					ViewBag.Error = "Nagłówki pliku się nie zgadzają, upewnij się że są one następujące: " + string.Join(", ", validHeaders);
+					return View();
+				}
+        while (!reader.EndOfStream) {
+          Klienci k = new();
+          string[] dane = reader.ReadLine()!.Trim().Split(",");
+          if (dane.Length != validHeaders.Length) {
+						ViewBag.Error = "Długość danych w linijce pliku nie odpowiada ilości nagłówków";
+						return View();
+					}
+          string pesel = dane[3];
+          int birthyear = int.Parse(dane[4]);
+          int płeć = int.Parse(dane[5]);
+					if (Klienci.ValidBirthYear(pesel, birthyear)) {
+						ViewBag.Error = "Rok urodzenia nie zgadza się z numerem PESEL";
+						return View();
+					}
+					if (Klienci.ValidPłeć(pesel, płeć)) {
+						ViewBag.Error = "Płeć nie zgadza się z numerem PESEL";
+						return View();
+					}
+					k.Id = int.Parse(dane[0]);
+          k.Name = dane[1];
+          k.Surname = dane[2];
+          k.PESEL = pesel;
+          k.BirthYear = birthyear;
+          k.Płeć = płeć;
+          ctx.Klienci.Add(k);
+        }
+        ctx.SaveChanges();
+      } else if (extension == ".xlsx") {
+        using var package = new ExcelPackage(File.OpenReadStream());
+        var worksheet = package.Workbook.Worksheets.FirstOrDefault();
+        if (worksheet == null) {
+					ViewBag.Error = "Plik jest pusty lub niepoprawny";
+					return View();
+				}
+        string[] headers = new string[validHeaders.Length];
+        for (int col = 1; col <= validHeaders.Length; col++) {
+          headers[col - 1] = worksheet.Cells[1, col].Text;
+        }
+				if (!Enumerable.SequenceEqual(headers, validHeaders)) {
+					ViewBag.Error = "Nagłówki pliku się nie zgadzają, upewnij się że są one następujące: " + string.Join(", ", validHeaders);
+					return View();
+				}
+        for (int row = 2; row <= worksheet.Dimension.End.Row; row++) {
+          Klienci k = new();
+          string id = worksheet.Cells[row, 1].Text;
+          if (
+            string.IsNullOrWhiteSpace(id) ||
+            string.IsNullOrWhiteSpace(worksheet.Cells[row, 6].Text)
+          ) {
+					  ViewBag.Error = $"Dane w rzędzie {row} nie są poprawne";
+					  return View();
+				  }
+					// Pesel jest czytano jako np: 1,234567891E+10, dlatego należy zamienić na double i spowrotem string
+					string pesel = double.Parse(worksheet.Cells[row, 4].Text).ToString();
+					int birthyear = int.Parse(worksheet.Cells[row, 5].Text);
+					int płeć = int.Parse(worksheet.Cells[row, 6].Text);
+					if (Klienci.ValidBirthYear(pesel, birthyear)) {
+						ViewBag.Error = "Rok urodzenia nie zgadza się z numerem PESEL";
+						return View();
+					}
+					if (Klienci.ValidPłeć(pesel, płeć)) {
+						ViewBag.Error = "Płeć nie zgadza się z numerem PESEL";
+						return View();
+					}
+
+					k.Id = int.Parse(id);
+          k.Name = worksheet.Cells[row, 2].Text;
+          k.Surname = worksheet.Cells[row, 3].Text; ;
+          k.PESEL = pesel;
+					k.BirthYear = birthyear;
+          k.Płeć = płeć;
+					ctx.Klienci.Add(k);
+        }
+				ctx.SaveChanges();
+			}
+      ViewBag.Success = "Pomyślnie zaimportowano klientów";
+      return View();
+    }
+
+    [HttpGet("Export")]
+    public IActionResult Export(int type) {
+      if (type == 1) {
+        IEnumerable<Klienci> klienci = ctx.Klienci.ToList().OrderBy(k => k.Id);
+        StringBuilder csv = new();
+        csv.AppendLine(string.Join(",", validHeaders));
+        foreach (Klienci k in klienci) {
+          csv.AppendLine($"{k.Id},{k.Name},{k.Surname},{k.PESEL},{k.BirthYear},{k.Płeć}");
+        }
+        byte[] bytes = Encoding.UTF8.GetBytes(csv.ToString());
+        return File(bytes, "text/csv", "klienci.csv");
+      } else if (type == 2) {
+				IEnumerable<Klienci> klienci = ctx.Klienci.ToList().OrderBy(k => k.Id);
+        using var package = new ExcelPackage();
+        var worksheet = package.Workbook.Worksheets.Add("Klienci");
+        
+        for (int i = 0; i < validHeaders.Length; i++) {
+          worksheet.Cells[1, i + 1].Value = validHeaders[i];
+        }
+        int row = 2;
+        foreach (Klienci k in klienci) {
+					worksheet.Cells[row, 1].Value = k.Id;
+					worksheet.Cells[row, 2].Value = k.Name;
+					worksheet.Cells[row, 3].Value = k.Surname;
+					worksheet.Cells[row, 4].Value = k.PESEL;
+					worksheet.Cells[row, 5].Value = k.BirthYear;
+					worksheet.Cells[row, 6].Value = k.Płeć;
+					row++;
+        }
+        byte[] bytes = package.GetAsByteArray();
+        return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "klienci.xlsx");
+			} else {
+        return RedirectToAction("Index");
+      }
+    }
+	}
 }
--- Models/Klienci.cs
+++ Models/Klienci.cs
@@ -12,5 +12,36 @@ namespace Zadanie_5.Models {
     public string PESEL { get; set; }
     public int BirthYear { get; set; }
     public int Płeć { get; set; }
+    
+    public static int GetBirthYear(string PESEL) {
+			if (PESEL.Length != 11) return 0;
+			int year = int.Parse(PESEL.Substring(0, 2));
+			int month = int.Parse(PESEL.Substring(2, 2));
+
+			if (month <= 12) {
+				year += 1900;
+			} else if (month <= 32) {
+				year += 2000;
+			} else if (month <= 52) {
+				year += 2100;
+			} else if (month <= 72) {
+				year += 1800;
+			}
+			return year;
+		}
+
+    public static int GetPłeć(string PESEL) {
+			if (PESEL.Length != 11) return 0;
+			return int.Parse(PESEL[9].ToString()) % 2 == 0 ? 1 : 2;
+		}
+
+    public static bool ValidBirthYear(string PESEL, int birthYear) {
+      if (PESEL.Length != 11) return false;
+      return GetBirthYear(PESEL) == birthYear;
+		}
+
+    public static bool ValidPłeć(string PESEL, int płeć) {
+      return GetPłeć(PESEL) == płeć;
+		}
   }
 }
